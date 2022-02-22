da10d7599aec4ce591c80539f32139ff1db19d11
ArthurDeSchutter <deschutterarthur1@gmail.com>
2022-02-22 10:52:54 +0100

    added foreach loop instead of long for loop

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -12,90 +12,90 @@ namespace csharp
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            foreach (var Item in Items)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Item.Name != "Aged Brie" && Item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (Items[i].Quality > 0)
+                    if (Item.Quality > 0)
                     {
                         //"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        if (Item.Name != "Sulfuras, Hand of Ragnaros")
                         {
-                            if (Items[i].Name == "Conjured Mana Cake")
+                            if (Item.Name == "Conjured Mana Cake")
                             {
-                                Items[i].Quality = Items[i].Quality - 2;
+                                Item.Quality = Item.Quality - 2;
                             }
                             else
                             {
-                                Items[i].Quality = Items[i].Quality - 1;
+                                Item.Quality = Item.Quality - 1;
                             }
                         }
                     }
                 }
                 else
                 {
-                    if (Items[i].Quality < 50)
+                    if (Item.Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        Item.Quality = Item.Quality + 1;
                         /*
                         "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
 	                    Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
 	                    Quality drops to 0 after the concert
                         */
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (Item.Name == "Backstage passes to a TAFKAL80ETC concert")
                         {
-                            if (Items[i].SellIn < 11)
+                            if (Item.SellIn < 11)
                             {
-                                if (Items[i].Quality < 50)
+                                if (Item.Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    Item.Quality = Item.Quality + 1;
                                 }
                             }
 
-                            if (Items[i].SellIn < 6)
+                            if (Item.SellIn < 6)
                             {
                                 //The Quality of an item is never more than 50
-                                if (Items[i].Quality < 50)
+                                if (Item.Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    Item.Quality = Item.Quality + 1;
                                 }
                             }
                         }
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (Item.Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    Items[i].SellIn = Items[i].SellIn - 1;
+                    Item.SellIn = Item.SellIn - 1;
                 }
 
                 //Once the sell by date has passed, Quality degrades twice as fast
-                if (Items[i].SellIn < 0)
+                if (Item.SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Item.Name != "Aged Brie")
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (Item.Name != "Backstage passes to a TAFKAL80ETC concert")
                         {
                             //The Quality of an item is never negative
-                            if (Items[i].Quality > 0)
+                            if (Item.Quality > 0)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (Item.Name != "Sulfuras, Hand of Ragnaros")
                                 {
-                                    Items[i].Quality = Items[i].Quality - 1;
+                                    Item.Quality = Item.Quality - 1;
                                 }
                             }
                         }
                         else
                         {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            Item.Quality = Item.Quality - Item.Quality;
                         }
                     }
                     else //"Aged Brie" actually increases in Quality the older it gets
                     {
                         //The Quality of an item is never more than 50
-                        if (Items[i].Quality < 50)
+                        if (Item.Quality < 50)
                         {
-                            Items[i].Quality = Items[i].Quality + 1;
+                            Item.Quality = Item.Quality + 1;
                         }
                     }
                 }
