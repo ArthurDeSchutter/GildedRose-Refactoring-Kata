0adbb300cd7e27d4646158e6833a17e52f33cae0
ArthurDeSchutter <deschutterarthur1@gmail.com>
2022-02-22 12:38:33 +0100

    added seperate functions for unique items

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -10,98 +10,109 @@ namespace csharp
             this.Items = Items;
         }
 
-        public bool isOverMaxQuality(int quality) => quality >= 50 ? false : true;
+        public bool isNotOverMaxQuality(int quality) => quality >= 50 ? false : true;
+
 
         public void UpdateQuality()
         {
             foreach (var Item in Items)
             {
-                if (Item.Name != "Aged Brie" && Item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                switch (Item.Name)
                 {
-                    if (Item.Quality > 0)
+                    case "Aged Brie":
+                        UpdateAgedBrie(Item);
+                        break;
+
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        UpdateBackstagePasses(Item);
+                        break;
+
+                    case "Conjured Mana Cake":
+                        UpdateConjured(Item);
+                        break;
+
+                    case "Sulfuras, Hand of Ragnaros":
+                        break;
+
+                    default:
+                        UpdateNormalItem(Item);
+                        break;
+                }
+            }
+        }
+
+        public void UpdateAgedBrie(Item item)
+        {
+            if (isNotOverMaxQuality(item.Quality))
+            {
+                item.Quality++;
+            }
+            item.SellIn--;
+        }
+
+        public void UpdateBackstagePasses(Item item)
+        {
+            if (item.SellIn < 11)
+            {
+                if (item.SellIn < 6)
+                {
+                    if (isNotOverMaxQuality(item.Quality))
                     {
-                        //"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-                        if (Item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            if (Item.Name == "Conjured Mana Cake")
-                            {
-                                Item.Quality = Item.Quality - 2;
-                            }
-                            else
-                            {
-                                Item.Quality--;
-                            }
-                        }
+                        item.Quality = item.Quality + 3;
                     }
                 }
                 else
                 {
-                    if (isOverMaxQuality(Item.Quality))
+                    if (isNotOverMaxQuality(item.Quality))
                     {
-                        Item.Quality++;
-                        /*
-                        "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
-	                    Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
-	                    Quality drops to 0 after the concert
-                        */
-                        if (Item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Item.SellIn < 11)
-                            {
-                                if (isOverMaxQuality(Item.Quality))
-                                {
-                                    Item.Quality++;
-                                }
-                            }
-
-                            if (Item.SellIn < 6)
-                            {
-                                //The Quality of an item is never more than 50
-                                if (isOverMaxQuality(Item.Quality))
-                                {
-                                    Item.Quality++;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Item.SellIn--;
-                }
-
-                //Once the sell by date has passed, Quality degrades twice as fast
-                if (Item.SellIn < 0)
-                {
-                    if (Item.Name != "Aged Brie")
-                    {
-                        if (Item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            //The Quality of an item is never negative
-                            if (Item.Quality > 0)
-                            {
-                                if (Item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Item.Quality--;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Item.Quality = Item.Quality - Item.Quality;
-                        }
-                    }
-                    else //"Aged Brie" actually increases in Quality the older it gets
-                    {
-                        //The Quality of an item is never more than 50
-                        if (isOverMaxQuality(Item.Quality))
-                        {
-                            Item.Quality++;
-                        }
+                        item.Quality = item.Quality + 2;
                     }
                 }
             }
+
+            if (item.SellIn < 1)
+            {
+                item.Quality = 0;
+            }
+
+            if (item.Quality > 50)
+            {
+                item.Quality = 50;
+            }
+            item.SellIn--;
         }
+
+        public void UpdateConjured(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                if (item.SellIn < 0)
+                {
+                    item.Quality = item.Quality - 4;
+                }
+                else
+                {
+                    item.Quality = item.Quality - 2;
+                }
+            }
+            item.SellIn--;
+        }
+
+        public void UpdateNormalItem(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                if (item.SellIn < 0)
+                {
+                    item.Quality = item.Quality - 2;
+                }
+                else
+                {
+                    item.Quality--;
+                }
+            }
+            item.SellIn--;
+        }
+
     }
 }
